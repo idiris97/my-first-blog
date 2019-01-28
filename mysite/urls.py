be8477c11580bcc3f5eae44735140bca024f7d19
idiris97 <idiris.sattarov@mail.ru>
2019-01-29 02:02:32 +0600

    change 2

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from blog.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
